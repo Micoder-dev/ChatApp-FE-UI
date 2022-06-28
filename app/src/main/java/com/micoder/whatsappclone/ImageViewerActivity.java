e664e5dfec4077b9ad30b26bb1953f738d3eef14
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-28 15:27:33 +0530

    imageView scale zoomin/zoomout added programmatically

--- app/src/main/java/com/micoder/whatsappclone/ImageViewerActivity.java
+++ app/src/main/java/com/micoder/whatsappclone/ImageViewerActivity.java
@@ -3,6 +3,8 @@ package com.micoder.whatsappclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -11,6 +13,9 @@ public class ImageViewerActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private String imageUrl;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float FACTOR = 1.0f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,26 @@ public class ImageViewerActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_viewer);
         imageUrl = getIntent().getStringExtra("url");
 
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
         Picasso.get().load(imageUrl).into(imageView);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            FACTOR *= detector.getScaleFactor();
+            FACTOR = Math.max(0.1f, Math.min(FACTOR, 10.0f));
+            imageView.setScaleX(FACTOR);
+            imageView.setScaleY(FACTOR);
+            return true;
+        }
+    }
+
 }
